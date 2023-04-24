9f48e35f89522be13b7580d5d05369e1f5966f66
viciossevsk <viciossevsk@gmail.com>
2023-04-24 19:23:19 +0300

    TZ_14 fix_1

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -9,51 +9,51 @@ import java.util.Collection;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
- Collection<Item> findAllByOwnerId(Long ownerId);
+    Collection<Item> findAllByOwnerId(Long ownerId);
 
- Collection<Item> findAllByNameIsLikeIgnoreCaseOrDescriptionIsLikeIgnoreCaseAndAvailableTrue(String queryForName,
-                                                                                             String queryForDescription);
+    Collection<Item> findAllByNameIsLikeIgnoreCaseOrDescriptionIsLikeIgnoreCaseAndAvailableTrue(String queryForName,
+                                                                                                String queryForDescription);
 
- void deleteByIdAndAndOwnerId(Long itemId, Long ownerId);
+    void deleteByIdAndAndOwnerId(Long itemId, Long ownerId);
 
- @Query(
-         "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
-                 " from Booking as b " +
-                 " where item in ?1" +
-                 " and CURRENT_TIMESTAMP < b.start" +
-                 " and b.status = 'APPROVED'" +
-                 " order by b.start"
- )
- Collection<BookingShortDto> findNextBookings(Collection<Item> items);
+    @Query(
+            "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
+                    " from Booking as b " +
+                    " where item in ?1" +
+                    " and CURRENT_TIMESTAMP < b.start" +
+                    " and b.status = 'APPROVED'" +
+                    " order by b.start"
+    )
+    Collection<BookingShortDto> findNextBookings(Collection<Item> items);
 
- @Query(
-         "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
-                 " from Booking as b " +
-                 " where item in ?1" +
-                 " and CURRENT_TIMESTAMP > b.start" +
-                 " and b.status = 'APPROVED'" +
-                 " order by b.start desc"
- )
- Collection<BookingShortDto> findLastBookings(Collection<Item> items);
+    @Query(
+            "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
+                    " from Booking as b " +
+                    " where item in ?1" +
+                    " and CURRENT_TIMESTAMP > b.start" +
+                    " and b.status = 'APPROVED'" +
+                    " order by b.start desc"
+    )
+    Collection<BookingShortDto> findLastBookings(Collection<Item> items);
 
- @Query(
-         "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
-                 " from Booking as b " +
-                 " where item = ?1" +
-                 " and CURRENT_TIMESTAMP < b.start" +
-                 " and b.status = 'APPROVED'" +
-                 " order by b.start"
- )
- Collection<BookingShortDto> findNextBookingsOfItem(Item item);
+    @Query(
+            "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
+                    " from Booking as b " +
+                    " where item = ?1" +
+                    " and CURRENT_TIMESTAMP < b.start" +
+                    " and b.status = 'APPROVED'" +
+                    " order by b.start"
+    )
+    Collection<BookingShortDto> findNextBookingsOfItem(Item item);
 
- @Query(
-         "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
-                 " from Booking as b " +
-                 " where item = ?1" +
-                 " and CURRENT_TIMESTAMP > b.start" +
-                 " and b.status = 'APPROVED'" +
-                 " order by b.start desc"
- )
- Collection<BookingShortDto> findLastBookingsOfItem(Item item);
+    @Query(
+            "select new ru.practicum.shareit.booking.dto.BookingShortDto(b.id, b.booker.id, b.item.id)" +
+                    " from Booking as b " +
+                    " where item = ?1" +
+                    " and CURRENT_TIMESTAMP > b.start" +
+                    " and b.status = 'APPROVED'" +
+                    " order by b.start desc"
+    )
+    Collection<BookingShortDto> findLastBookingsOfItem(Item item);
 
 }
