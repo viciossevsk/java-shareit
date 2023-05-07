313a3b34f3eeaf65c880a68d3e3aff2d897aedad
viciossevsk <viciossevsk@gmail.com>
2023-05-07 04:01:57 +0300

    tests service part 2

--- src/test/java/ru/practicum/shareit/item/item/ItemRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/item/ItemRepositoryTest.java
@@ -1,33 +1,19 @@
 package ru.practicum.shareit.item.item;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
-import ru.practicum.shareit.booking.BookingServiceImpl;
-import ru.practicum.shareit.booking.BookingStatus;
-import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.item.ItemServiceImpl;
-import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.repository.ItemRepository;
-import ru.practicum.shareit.user.dto.UserDto;
 
-import java.awt.print.Book;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.practicum.shareit.otherFunction.AddvansedFunctions.getPage;
 
 @Transactional
 @SpringBootTest
