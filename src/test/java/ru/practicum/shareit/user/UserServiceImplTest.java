449f628bd660c55be6dfcaa24dd200bf1eb4b85b
viciossevsk <viciossevsk@gmail.com>
2023-05-07 03:00:33 +0300

    all service test

---
+++ src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -0,0 +1,86 @@
+package ru.practicum.shareit.user;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Sql(value = "/testSchema.sql")
+public class UserServiceImplTest {
+    private final UserServiceImpl mockUserServiceImpl;
+    UserDto requestUserDto;
+    UserDto responseUserDto;
+    UserDto responseUserDto1;
+    long userId = 1L;
+
+    @BeforeEach
+    void setUp() {
+        requestUserDto = UserDto.builder()
+                .name("vasya")
+                .email("vasya@mail.ru")
+                .build();
+
+        responseUserDto = UserDto.builder()
+                .id(4L)
+                .name("vasya")
+                .email("vasya@mail.ru")
+                .build();
+
+        responseUserDto1 = UserDto.builder()
+                .id(1L)
+                .name("user_1")
+                .email("user_1@mail.ru")
+                .build();
+    }
+
+    @Test
+    void createUser_checkData() {
+
+        UserDto userDto = mockUserServiceImpl.createUser(requestUserDto);
+        assertEquals(responseUserDto.toString(), userDto.toString());
+
+    }
+
+    @Test
+    void getAllUsers_checkData() {
+
+        List<UserDto> userDtos = mockUserServiceImpl.getAllUsers();
+        assertEquals(3, userDtos.size());
+    }
+    @Test
+    void getUserById_checkData() {
+
+        UserDto userDto = mockUserServiceImpl.getUserById(userId);
+        assertEquals(responseUserDto1.toString(), userDto.toString());
+    }
+    @Test
+    void updateUser_checkData() {
+        requestUserDto.setName("Update name");
+
+        UserDto userDto = mockUserServiceImpl.updateUser(requestUserDto, userId);
+        assertEquals(requestUserDto.toString(), userDto.toString());
+    }
+    @Test
+    void deleteUserById_checkData() {
+
+        mockUserServiceImpl.deleteUserById(userId);
+
+        List<UserDto> userDtos = mockUserServiceImpl.getAllUsers();
+        assertEquals(2, userDtos.size());
+    }
+
+
+}
